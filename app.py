6970d7f245df45f726bd70e0829b794b52be71f3
RQuicho <raymondquicho@gmail.com>
2023-05-20 22:51:08 -0700

    setting up html templates and starting to build User class

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+app.config['SECRET KEY'] = 'flasksqlalchemyexercise'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
+
 db.create_all()
+
+
+@app.route('/')
+def list_all_users():
+	"""Shows list of all users in db"""
+
+	users = User.query.all()
+	return render_template('list.html', users=users)
