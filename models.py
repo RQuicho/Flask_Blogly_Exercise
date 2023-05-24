41e209bb1f5181061bc458c06405d5bb2e0b29ad
RQuicho <raymondquicho@gmail.com>
2023-05-23 17:03:29 -0700

    added Post class and created a foreign key that connects to User table

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -14,7 +15,23 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	first_name = db.Column(db.String(50), nullable=False)
 	last_name = db.Column(db.String(50), nullable=False)
-	image_url = db.Column(db.String, nullable=False)
+	image_url = db.Column(db.Text, nullable=False)
 
+	def __repr__(self):
+		return f"< User {self.first_name} {self.last_name} {self.image_url} >"
+
+class Post(db.Model):
+	__tablename__ = 'posts'
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	title = db.Column(db.Text, nullable=False)
+	content = db.Column(db.Text, nullable=False)
+	created_at = db.Column(db.DateTime, default=datetime.utcnow)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+	user = db.relationship('User', backref='users')
+
+	def __repr__(self):
+		return f"< Post {self.title} {self.content} {self.created_at} {self.user_id} >"
 
 
